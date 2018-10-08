24adee1c422a5d83f8c7be142b7b4669897ddf07
Marcos Ruiz Garcia <sobrenombre@gmail.com>
2018-10-08 14:16:45 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,99 @@
+# SignuCA
+
+Certificate Authority for SignuApp
+
+We can:
+ - Add certificates using CSR
+ - Revoke certificates
+ - Get CRL
+ - Check certificates using OCSP (in future...)
+ - Get public certificate of SignuCA
+
+### Set the structure on point
+
+I recommend use the native command prompt of your OS to follow the following steps with Admin privileges or Git bash
+
+You must stay in SignuTimes/ and create this directory if it does not exist
+
+~~~
+mkdir openssl
+cd openssl
+mkdir ca
+cd ca
+mkdir private
+mkdir newcerts
+touch index.txt
+echo 01 > serial
+cd ..
+~~~
+
+### Warning
+
+Copy openssl.cnf of `C:\Program Files\Git\mingw64\ssl` to openssl/ and edit this line `# extendedKeyUsage/` into this `extendedKeyUsage/` and edit dir to `./ca`.
+Edit [tsa_config1]:
+
+- In `dir` put TSA absolute root directory
+- In `digest` put sha256 and every digest that you use
+
+Edit [ca_config]:
+
+- In `dir` put CA absolute root directory
+
+
+
+### Generate a Private and Public Key for my CA
+
+~~~
+openssl genrsa -out ca/private/cakey.pem 4096
+openssl req -new -x509 -days 3650 -key ca/private/cakey.pem -out ca/newcerts/cacert.pem -subj "/C=ES/ST=Zaragoza/L=Zaragoza/O=Signu/OU=Signu/CN=Signu/emailAddress=signu.app@gmail.com"
+~~~
+
+If you have this problem: unable to write 'random state'
+The solution is: use terminal with admin privileges
+
+~~~
+cp ca/newcerts/cacert.pem ca
+~~~
+
+# Certificate Revocation List (CRL)
+
+First we create some things:
+~~~
+cd ca
+echo 01 > crlnumber
+mkdir crl
+cd ..
+~~~
+
+With this we create a CRL and check it:
+~~~
+openssl ca -config openssl.cnf -gencrl -out ca/crl/ca.crl
+openssl crl -in ca/crl/ca.crl -noout -text
+~~~
+
+# SignuOCSP (Info)
+
+## What it is an OCSP
+
+Instead using a CRT (Certificate Revocation List) we are going to use a service OCSP (Online Certificate Status Protocol) to know if a certificate is valid or is revoked.
+Because its more effective, with OCSP we can check only one certificate without download all CRT.
+
+This service uses RFC2560 which specification is in this [link](https://www.ietf.org/rfc/rfc2560.txt).
+
+~~~
+openssl req -config ./openssl.cnf -new -nodes -out ca.csr -keyout ca.key -extensions v3_ocsp -subj "/C=ES/ST=Zaragoza/L=Zaragoza/O=Signu/OU=Signu/CN=Signu/emailAddress=signu.app@gmail.com"
+openssl ca -config ./openssl.cnf -in tsacert.csr -out ca.crt -extensions v3_ocsp
+openssl req -config ./openssl.cnf  -new -nodes -out dummy.csr -keyout dummy.key -subj "/C=ES/ST=Zaragoza/L=Zaragoza/O=Signu/OU=Signu/CN=Signu/emailAddress=signu.app@gmail.com"
+openssl ca -config ./openssl.cnf -in dummy.csr -out dummy.crt
+openssl ocsp -index index.txt -port 8888 -CA cacert.pem -rsigner ca.crt -rkey ca.key -text -out log.txt
+openssl ocsp -index /etc/pki/CA/index.txt -port 8888 -rsigner ca.isrlabs.net.crt -rkey ca.isrlabs.net.key -CA /etc/pki/CA/cacert.pem -text -out log.txt
+~~~
+
+
+# Update .gitignore
+
+~~~
+git rm -r --cached .
+git add .
+git commit -m "fixed untracked files"
+~~~
---
+++ routes/index.js
@@ -0,0 +1,135 @@
+const express = require('express');
+var router = express.Router();
+const http = require('http');
+const fs = require('file-system');
+const ssl = 'openssl';
+const path = require('path');
+const exec = require('child_process').exec;
+var HttpStatus = require('http-status-codes');
+
+const cacert = path.resolve(__dirname, '../openssl/ca/cacert.pem');
+const cakey = path.resolve(__dirname, '../openssl/ca/private/cakey.pem');
+const config = path.resolve(__dirname, '../openssl/openssl.cnf');
+const crl = path.resolve(__dirname, '../openssl/ca/crl/ca.crl');
+const crlPem = path.resolve(__dirname, '../openssl/ca/crl/ca.crl.pem');
+
+router.get('/', function (req, res) {
+    res.send('Welcome to Signu CA');
+});
+
+router.post('/cert', processCSR);
+
+router.get('/ocsp', function (req, res) {
+    res.send("Welcome to GET OCSP");
+});
+router.post('/ocsp', postOCSP);
+
+router.get('/ca.crl', getCRL);
+
+router.get('/revoke', revokeCert);
+
+
+function processCSR(req, res, next) {
+    req.on('data', function (data) {
+        fs.writeFileSync('file.csr', data);
+    });
+    req.on('end', function () {
+        generateCSRReply('file.csr', function(err, reply){
+            if (err) {
+                res.status(500).send("Internal error");
+            } else {
+                res.header('Content-Disposition', 'attachment; filename=file.pem');
+                res.download(reply, 'file.pem');
+            }
+        });
+    });
+}
+
+function generateCSRReply(query, callback) {
+    const dirname = path.dirname(query);
+    const basename = path.basename(query, path.extname(query));
+    const reply = path.resolve(dirname, `${basename}.pem`);
+    const queryRoute = path.resolve(dirname, `${basename}.csr`);
+    // openssl ca -in personalcert.csr -config openssl.cnf -out ca/newcerts/personalcert.pem
+    const cmd = `${ssl} ca -batch -in ${queryRoute} -config ${config} -out ${reply}`;
+    const child = exec(cmd, (err, stdout, stderr) => {
+        if (err) {
+            callback(err);
+        } else {
+            console.log(stdout);
+            callback(null, reply);
+        }
+    });
+}
+
+
+/**
+ * Not tested
+ * @param req
+ * @param res
+ * @param next
+ */
+function revokeCert(req, res, next) {
+    // Get cert
+    req.on('data', function (data) {
+        fs.writeFileSync('certToRevoke.pem', data);
+    });
+    req.on('end', function () {
+        // Revoke certificate
+        // openssl ca -config openssl.cnf -revoke ca/personalcert.pem -keyfile ca/private/cakey.pem -cert ca/cacert.pem
+        var cmd = `${ssl} ca -config ${config} -revoke certToRevoke.pem -keyfile ${cakey} -cert ${cacert}`;
+        const child = exec(cmd, (err, stdout, stderr) => {
+            if (err) return next(err);
+            res.status(HttpStatus.OK).send();
+        });
+    });
+}
+
+function getCRL(req, res, next) {
+    // Update CRL
+    // openssl ca -config openssl.cnf -gencrl -out ca/crl/ca.crl.pem
+    // openssl crl -inform PEM -in ca/crl/ca.crl.pem -outform DER -out ca/crl/ca.crl
+    var cmd = `${ssl} ca -config ${config} -gencrl -out ${crlPem}`;
+    const child = exec(cmd, (err, stdout, stderr) => {
+        if (err) return next(err);
+        cmd = `${ssl} crl -inform PEM -in ${crlPem} -outform DER -out ${crl}`;
+        const child2 = exec(cmd, (err, stdout, stderr) => {
+            if (err) return next(err);
+            // Send CRL
+            res.status(200);
+            res.download(crl, 'ca.crl');
+        });
+    });
+}
+
+var serviceCallback = function (response) {
+    return function (err, obj) {
+        console.log(response);
+        if (err) {
+            response.send(500);
+        } else {
+            response.send(obj);
+        }
+    }
+};
+
+/**
+ * check this with: openssl ocsp
+ */
+function postOCSP(req, res) {
+    if (req.header('content-type') != 'application/ocsp-request') {
+        res.status(HttpStatus.BAD_REQUEST).send("Bad Request");
+    } else {
+        req.on('data', function (data) {
+            fs.writeFileSync('file.ocsp', data);
+            console.log(data);
+        });
+        req.on('end', function () {
+            console.log("ocsp");
+            res.header('content-type', 'application/ocsp-response');
+            res.status(200).send("BLA");
+        });
+    }
+}
+
+module.exports = router;
