5df1bb870113b8efeb9168776537c543c945f72b
marcos ruiz <sobrenombre@gmail.com>
2018-12-26 13:32:13 +0100

    heroku still not has cakey.pem

--- test/setTest.js
+++ test/setTest.js
@@ -14,7 +14,7 @@ var HttpStatus = require('http-status-codes');
 var request = require('supertest');
 
 // openssl should be in Enviroment variables
-const ssl = 'openssl.exe';
+const ssl = 'openssl';
 const path = require('path');
 const exec = require('child_process').exec;
 var random = generateRandomString(5);
@@ -54,23 +54,29 @@ describe('Certificates: creation and revocation', function () {
     it('It should create a new user certificate', function (done) {
         this.timeout(5000);
         // openssl genrsa 4096 > ca/private/personalkey.pem
-        var destKey = __dirname + '\\testFiles\\myKey.pem';
+        var destKey = __dirname + '/testFiles/myKey.pem';
         var cmd = `${ssl} genrsa 4096 > ${destKey}`;
         const child = exec(cmd, (err, stdout, stderr) => {
-            if (err) return;
+            if (err) {
+                console.log(stderr);
+                return;
+            }
             // openssl req -new -key ca/private/personalkey.pem -out personalcert.csr -subj "//C=ES\ST=Zaragoza\L=Zaragoza\O=Signu\OU=Signu\CN=SignuPersonal\emailAddress=sobrenombre@gmail.com"
-            var destCsr = __dirname + '\\testFiles\\myCert.csr';
+            var destCsr = __dirname + '/testFiles/myCert.csr';
             cmd = `${ssl} req -new -key ${destKey} -out ${destCsr} -subj "/C=ES/ST=Zaragoza/L=Zaragoza/O=Signu/OU=Signu/CN=SignuTest/emailAddress=test${random}@gmail.com"`
             const child2 = exec(cmd, (err, stdout, stderr) => {
-                if (err) return;
+                if (err) {
+                    console.log(stderr);
+                    return;
+                }
                 var agent = chai.request.agent(server);
                 agent.post('/cert')
                     .attach("csr", fs.readFileSync(destCsr), "csr")
                     .buffer()
                     .parse(binaryParser)
                     .end(function (err, res) {
-                        var path = __dirname + "\\testFiles";
-                        fs.writeFileSync(path + "\\myCert.pem", res.body);
+                        var path = __dirname + "/testFiles";
+                        fs.writeFileSync(path + "/myCert.pem", res.body);
                         res.should.have.status(HttpStatus.OK);
                         done();
                     });
@@ -79,7 +85,7 @@ describe('Certificates: creation and revocation', function () {
     });
 
     it('It should revoke a user certificate', function (done) {
-        var myCertRoute = __dirname + "\\testFiles\\myCert.pem";
+        var myCertRoute = __dirname + "/testFiles/myCert.pem";
         var agent = chai.request.agent(server);
         agent.get('/revoke')
             .attach("pem", fs.readFileSync(myCertRoute), "pem")
@@ -92,12 +98,12 @@ describe('Certificates: creation and revocation', function () {
     it('It should create, revoke a certificate', function (done) {
         this.timeout(10000);
         // openssl genrsa 4096 > ca/private/personalkey.pem
-        var destKey = __dirname + '\\testFiles\\myKey.pem';
+        var destKey = __dirname + '/testFiles/myKey.pem';
         var cmd = `${ssl} genrsa 4096 > ${destKey}`;
         const child = exec(cmd, (err, stdout, stderr) => {
             if (err) return;
             // openssl req -new -key ca/private/personalkey.pem -out personalcert.csr -subj "//C=ES\ST=Zaragoza\L=Zaragoza\O=Signu\OU=Signu\CN=SignuPersonal\emailAddress=sobrenombre@gmail.com"
-            var destCsr = __dirname + '\\testFiles\\myCert.csr';
+            var destCsr = __dirname + '/testFiles/myCert.csr';
             cmd = `${ssl} req -new -key ${destKey} -out ${destCsr} -subj "/C=ES/ST=Zaragoza/L=Zaragoza/O=Signu/OU=Signu/CN=SignuTest/emailAddress=test${random}@gmail.com"`
             const child2 = exec(cmd, (err, stdout, stderr) => {
                 if (err) return;
@@ -108,8 +114,8 @@ describe('Certificates: creation and revocation', function () {
                     .buffer()
                     .parse(binaryParser)
                     .end(function (err, res) {
-                        var path = __dirname + "\\testFiles";
-                        var myCertRoute = __dirname + "\\testFiles\\myCert.pem";
+                        var path = __dirname + "/testFiles";
+                        var myCertRoute = __dirname + "/testFiles/myCert.pem";
                         fs.writeFileSync(myCertRoute, res.body);
                         res.should.have.status(HttpStatus.OK);
                         // Revoke certificate
@@ -127,12 +133,12 @@ describe('Certificates: creation and revocation', function () {
     it('It should create, revoke a certificate and check CRL', function (done) {
         this.timeout(20000);
         // openssl genrsa 4096 > ca/private/personalkey.pem
-        var destKey = __dirname + '\\testFiles\\myKey.pem';
+        var destKey = __dirname + '/testFiles/myKey.pem';
         var cmd = `${ssl} genrsa 4096 > ${destKey}`;
         const child = exec(cmd, (err, stdout, stderr) => {
             if (err) return;
             // openssl req -new -key ca/private/personalkey.pem -out personalcert.csr -subj "//C=ES\ST=Zaragoza\L=Zaragoza\O=Signu\OU=Signu\CN=SignuPersonal\emailAddress=sobrenombre@gmail.com"
-            var destCsr = __dirname + '\\testFiles\\myCert.csr';
+            var destCsr = __dirname + '/testFiles/myCert.csr';
             cmd = `${ssl} req -new -key ${destKey} -out ${destCsr} -subj "/C=ES/ST=Zaragoza/L=Zaragoza/O=Signu/OU=Signu/CN=SignuTest/emailAddress=test${random}@gmail.com"`
             const child2 = exec(cmd, (err, stdout, stderr) => {
                 if (err) return;
@@ -143,8 +149,8 @@ describe('Certificates: creation and revocation', function () {
                     .buffer()
                     .parse(binaryParser)
                     .end(function (err, res) {
-                        var path = __dirname + "\\testFiles";
-                        var myCertRoute = __dirname + "\\testFiles\\myCert.pem";
+                        var path = __dirname + "/testFiles";
+                        var myCertRoute = __dirname + "/testFiles/myCert.pem";
                         fs.writeFileSync(myCertRoute, res.body);
                         res.should.have.status(HttpStatus.OK);
                         // Revoke certificate
@@ -157,7 +163,7 @@ describe('Certificates: creation and revocation', function () {
                                     .parse(binaryParser)
                                     .end(function (err, res) {
                                         res.should.have.status(HttpStatus.OK);
-                                        var pathCRL = __dirname + '\\testFiles\\ca.crl';
+                                        var pathCRL = __dirname + '/testFiles/ca.crl';
                                         fs.writeFileSync(pathCRL, res.body);
                                         // Check certificate is revoked
                                         cmd = `${ssl} crl -inform DER -text -noout -in ${pathCRL}`;
@@ -181,7 +187,7 @@ describe('Certificates: creation and revocation', function () {
 describe('GET CA Cert', function (done) {
     it('It shold get the CA Cert', function (done) {
         var agent = chai.request.agent(server);
-        var pathCaCert = __dirname + '\\testFiles\\cacert.pem';
+        var pathCaCert = __dirname + '/testFiles/cacert.pem';
         agent.get('/cacert')
             .buffer()
             .parse(binaryParser)
@@ -195,7 +201,7 @@ describe('GET CA Cert', function (done) {
 
 describe('CRL', function (done) {
     it('It should get a CRL', function (done) {
-        var pathCRL = __dirname + '\\testFiles\\ca.crl';
+        var pathCRL = __dirname + '/testFiles/ca.crl';
         var agent = chai.request.agent(server);
         agent.get('/ca.crl')
             .buffer()
@@ -207,8 +213,8 @@ describe('CRL', function (done) {
             });
     });
     it('It should get a CRL and check it', function (done) {
-        var destCRLDER = __dirname + '\\testFiles\\crl.der';
-        var destCRLPEM = __dirname + '\\testFiles\\crl.pem';
+        var destCRLDER = __dirname + '/testFiles/crl.der';
+        var destCRLPEM = __dirname + '/testFiles/crl.pem';
         var agent = chai.request.agent(server);
         agent.get('/ca.crl')
             .buffer()
